543fc51640f1be437d1a9b46e28d3b32f8fb8636
Icebog,Hsieh <jcjtku@hotmail.com>
2016-08-30 18:38:32 +0800

    Test push

--- src/main/java/com/icebogstudio/hellospring/HomeController.java
+++ src/main/java/com/icebogstudio/hellospring/HomeController.java
@@ -14,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
-//@Controller
+
+@Controller
+@RequestMapping("/")
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
@@ -22,7 +24,7 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-//	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "home" ,method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
